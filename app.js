abac00743ac9af0de9f232f28ba58b1abfae3dcb
katiekoschland <katie.koschland@ft.com>
2017-10-05 17:09:17 +0100

    Temperature displayed to client

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 const path = require('path');
-//
+// const weather = require( path.resolve( __dirname, "./weather.js" ));
+
 // To handle HTTP POST reuest in express, you need to install the module body-parsser. It extracts the entire body portion of an incoming request stream
 // and exposes it on req.body
 const bodyParser = require('body-parser');
@@ -26,12 +27,25 @@ app.set('view engine', 'html');
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
-  res.sendFile(__dirname + '/views/layouts/main.html');
-
+  res.render(__dirname + '/views/layouts/main.html');
 });
 
 app.get('/weather', (req, res) => {
-  res.send('This page will show us the weather');
+  const request = require('request');
+  const apiKey = '948bcac04c4c2646c9fcd6f1d5d4b4d0';
+  const city = 'london';
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  request(url, function (err, response, body) {
+    if(err){
+      console.log('error:');
+    } else {
+      let weather = JSON.parse(body);
+      let celsius = (weather.main.temp - 273.15).toFixed(2);
+      let message = `It's ${celsius} degrees in ${weather.name}!`;
+      res.send(message);
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5000;
